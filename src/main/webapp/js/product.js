44af944691e36d9d8d1bd8a23f0c17c0e9ba5d8a
Yaroslav Tsebro <yaroslav.cebro@gmail.com>
2023-01-25 20:01:30 +0200

    minor changes

---
+++ src/main/webapp/js/product.js
@@ -0,0 +1,26 @@
+function addPhotoFolder(){
+        let root = document.getElementById("photos")
+        var aTag = document.createElement("br");
+        var file = document.createElement("input");
+        file.name = 'file';
+        file.type = 'file';
+        root.appendChild(aTag);
+        root.appendChild(file);
+}
+
+function deletePhotoFolder(){
+    let root = document.getElementById("photos");
+    root.removeChild(root.lastChild);
+    root.removeChild(root.lastChild);
+}
+
+function addCharacteristic(){
+    let node = document.getElementById("characteristic");
+    let root = document.getElementById("characteristics");
+    let newElem = node.cloneNode();
+    root.appendChild(newElem);
+}
+function deleteCharacteristic(){
+    let root = document.getElementById("characteristics");
+    root.removeChild(root.lastChild);
+}
